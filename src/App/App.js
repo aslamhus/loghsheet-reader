fe916d69fdd236f39491b9eb5d03612c4adf09eb
aslamhus <aslam.percy.husain@gmail.com>
2023-03-19 15:16:02 -0700

    Feature: add react routes

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import LogsheetReader from './Components/LogsheetReader';
 import Shows from './Components/Shows/Shows';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 // import * as html2pdf from 'html-to-pdf-js';
@@ -8,9 +9,16 @@ import './app.css';
 
 export default function App() {
   return (
-    <div className="app">
-      <LogsheetReader />
-      <Shows />
-    </div>
+    <React.StrictMode>
+      <div className="app">
+        <Router basename="/logsheet-reader">
+          <Routes>
+            <Route path="/" element={<LogsheetReader />}></Route>
+            <Route path="/reader" element={<LogsheetReader />}></Route>
+            <Route path="/shows" element={<Shows />}></Route>
+          </Routes>
+        </Router>
+      </div>
+    </React.StrictMode>
   );
 }
